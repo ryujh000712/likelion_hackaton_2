f59bc2a050cd5f3686eca27b82f729b8493d7c33
ryujh000712 <paperflake121@gmail.com>
2019-08-10 02:20:17 +0900

    url_3

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-=======
-from django.contrib import admin
->>>>>>> 70c0aa876a0126665703ae235bac6e64812cd055
+from django.urls import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-=======
-  path('',views.portfolio, name = 'portfolio'),
->>>>>>> 70c0aa876a0126665703ae235bac6e64812cd055
+  pah('',views.portfolio, name = 'portfolio'),
+
 ]
