43d28694e6aefbae955cb1747a0d96ffbc3b64ea
ryujh000712 <paperflake121@gmail.com>
2019-08-10 01:28:59 +0900

    heroku?

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn likelion_hackaton_2.wsgi --log-file -
--- likelion_hackathon/settings.py
+++ likelion_hackathon/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e%$ohn#@a6m_$&yb)#!(2jaryc%84o=^-x55=vmp+i(8du#vm#'
+# SECRET_KEY = 'e%$ohn#@a6m_$&yb)#!(2jaryc%84o=^-x55=vmp+i(8du#vm#'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'e%$ohn#@a6m_$&yb)#!(2jaryc%84o=^-x55=vmp+i(8du#vm#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['*']
 
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,3 +137,9 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
