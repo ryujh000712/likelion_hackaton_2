0b9e2177ef004865f3359b9973e042ea2bd65ad1
hyeoong96 <brain_power@naver.com>
2019-08-09 22:15:53 +0900

    wordcount_2

--- likelion_hackathon/urls.py
+++ likelion_hackathon/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import wordconter.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',wordconter.views.wchome,name="wchome"),
 ]
