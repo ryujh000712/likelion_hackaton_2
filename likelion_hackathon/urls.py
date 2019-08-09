80a7d5c399f8ca69f908e49f1b96f97d75b76dbe
sunshane6726 <sunshane6726@gmail.com>
2019-08-09 23:36:07 +0900

    test_03

--- likelion_hackathon/urls.py
+++ likelion_hackathon/urls.py
@@ -20,11 +20,11 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 #import blog.views
-import portfolio.views
+#import portfolio.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', portfolio.views.portfolio)
     
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
