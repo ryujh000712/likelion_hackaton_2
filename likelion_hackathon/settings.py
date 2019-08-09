aaef13d59ee8c293fe9fee9c23a4c4bf12146acf
ryujh000712 <48249318+ryujh000712@users.noreply.github.com>
2019-08-10 00:56:40 +0900

    Update settings.py

--- likelion_hackathon/settings.py
+++ likelion_hackathon/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'wordconter.apps.WordconterConfig',
 
-    'portfolio.apps.PortfolioConfig',
 
     'main.apps.MainConfig',
     'blog.apps.BlogConfig',
