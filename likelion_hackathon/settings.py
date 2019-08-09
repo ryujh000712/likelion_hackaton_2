f5ac6a93e7239bdefea9e3d6cb91bcfd7689f88a
ryujh000712 <paperflake121@gmail.com>
2019-08-09 21:52:50 +0900

    static and blog_app

--- likelion_hackathon/settings.py
+++ likelion_hackathon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e%$ohn#@a6m_$&yb)#!(2jaryc%84o=^-x55=vmp+i(8du#vm#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,5 +116,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'blog', 'static')
+]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
